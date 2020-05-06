af791a73395791e33b69042039d6f8e2d0adaa30
darknight404 <josephgerald404@gmail.com>
2020-05-06 15:41:22 +0300

    error handling

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:eagle@127.0.0.1:5432/db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+
 
--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 set FLASK_APP=wsgi.py
+set FLASK_DEBUG=0
 set APP_CONFIG_FILE=config.py
 flask run
