ede33164ccc58cb833bea433f3375bf6ecf33d95
darknight404 <josephgerald404@gmail.com>
2020-05-06 13:42:00 +0300

    user login

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo,Length
+from .models import User
 
 class LoginForm(FlaskForm):
+    """User Login Form."""
+
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class SignupForm(FlaskForm):
+    """User Signup Form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Length(min=6), Email(message="Enter a valid email.")])
+    password = PasswordField('Password', validators=[DataRequired(message="Please enter a password."),Length(min=6, message="Select a stronger password.")])
+    password2 = PasswordField(
+        'Confirm Your Password', validators=[DataRequired(), EqualTo("password", message="Passwords must match.")])
+    submit = SubmitField('Signup!')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from blog import db
+from blog import db,login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
     """User account model."""
 
     ___tablename__="Users"
@@ -16,6 +17,15 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     """Posts model."""
