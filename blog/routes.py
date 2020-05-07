e4aeffabf1942546d893c09123839cd795e0dd43
darknight404 <josephgerald404@gmail.com>
2020-05-07 18:28:34 +0300

    bootstrap added

--- blog/routes.py
+++ blog/routes.py
@@ -31,7 +31,7 @@ def index():
         if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title='Home', posts=posts.items, next_url=next_url,
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url,
                            prev_url=prev_url)
 
 @app.route('/explore')
--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 set FLASK_APP=wsgi.py
-set FLASK_DEBUG=0
+set FLASK_DEBUG=1
 set MAIL_SERVER = localhost
 set MAIL_PORT = 8025
 set APP_CONFIG_FILE=config.py
