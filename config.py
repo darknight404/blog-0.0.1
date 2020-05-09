d894ca6ec297359fa7b3ac42eaeca16cf22f3a0c
darknight404 <josephgerald404@gmail.com>
2020-05-09 16:26:15 +0300

    app structure

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
@@ -9,5 +12,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = os.environ.get('ADMINS')
     POSTS_PER_PAGE = 25
